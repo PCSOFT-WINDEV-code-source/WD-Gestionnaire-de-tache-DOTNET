085f7625bdd97bcd296b3d56bcdbae0e60a37be3
PCSOFT-WINDEV <appli@pcsoft.fr>
2023-11-15 12:19:11 +0100

    Initial commit

---
+++ exinfos.ex
@@ -0,0 +1,9 @@
+[INFOS]
+gaf=0
+title=WD Gestionnaire de tache DOTNET
+image=description.gif
+description=description.rtf
+source=1
+preview=preview.png
+[OBJECT]
+WD Gestionnaire de tache DOTNET.WDP=1
